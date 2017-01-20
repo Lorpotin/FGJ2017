3a7a57407cd3c8093bc7adb9d8d790112d00bca8
Tatu Huttunen <tatu.huttunen@lut.fi>
2017-01-21 01:01:36 +0200

    gul

---
+++ server/static/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    
+    socket = io("localhost:8000");
+
+    result = {
+        msg: "Jotain shittii",
+        nick : ""
+    };
+
+    socket.on('connect', function() {
+        console.log("connection to socket established!");
+    });
+
+    $("#button").click(function() {
+        myFunction();
+    });
+    var myFunction = function() {
+        if($("input").val().length < 3 || $("input").val().length > 8) {
+            alert("give nickname of 3-8 chars");
+            return;
+        }
+        result.nick = $('input').val();
+        vari = "jotain shittii lähtee clientille";
+        socket.emit('spawn new powur', result);
+    };
+    
+});
