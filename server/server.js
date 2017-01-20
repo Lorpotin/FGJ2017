45f7c06a9067ccd29c9c34a4b09da53434ccb5a0
Otto Laitinen <lorpotin@hotmail.com>
2017-01-20 23:42:38 +0200

    Yes

--- server/server.js
+++ server/server.js
@@ -1,36 +1,37 @@
 var util = require("util"),
-    io = require("socket.io"),
+express = require('express');
+app = express(),
+io = require("socket.io");
 
 var socket, players;
 
+var PORT = 3000;
+
 players = [];
 
-function init() 
-{
+function init()  {
     //Set socket server to listen to a port
     socket = io.listen(8000);
    	setEventHandlers();
 };
 
-function setEventHandlers()
-{
+function setEventHandlers() {
 	//listens for new connections to Socket.IO
     socket.sockets.on("connection", onSocketConnection);
 };
 
 //The client variable is passed to onSocketConnection and is used to identify the player that just connected to the game.
-function onSocketConnection(client) 
-{
+function onSocketConnection(client) {
 	//Each player is identified using a unique client.id number. We'll use this later when communicating game updates to other players. 
 	//For now the util.log line will output the client identification number to the terminal on connection.
     util.log("New player has connected: "+client.id);
     //The client.on event listeners are used to detect when a player has either disconnected or sent a message to the server.
     client.on("disconnect", onClientDisconnect);
     client.on("new player", onNewPlayer);
+    client.on("spawn new powur", spawnNewPowur);
 };
 
-function onClientDisconnect() 
-{
+function onClientDisconnect() {
     util.log("Player has disconnected: "+this.id);
 
    //gets the correct id
@@ -48,4 +49,25 @@ function onClientDisconnect()
 	this.broadcast.emit("remove player", {id: this.id});
 
 };
+
+function spawnNewPowur() {
+
+}
+
+var playerById = function(id)  {
+    var i;
+    for (i = 0; i < players.length; i++)
+    {
+        if (players[i].id == id)
+        return players[i];
+    };
+
+    return false;
+};
+app.use(express.static('static'));
+
+app.listen(PORT, function() {
+    console.log('LISTENING....');
+    init();
+});
 
