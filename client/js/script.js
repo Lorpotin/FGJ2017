a4013a4fff6590cd5a45f2dacd59cc50b50c1e6b
rhalsas <rasmus.halsas@lut.fi>
2017-01-21 15:12:17 +0200

    -simple darknessmask

--- client/js/script.js
+++ client/js/script.js
@@ -7,24 +7,28 @@ var prevScore = -1;
 var scoreText = null;
 var player = null;
 var highScoreListText = [];
-var game = new Phaser.Game(GAME_HEIGHT,GAME_WIDTH, Phaser.CANVAS, '',{ preload: preload, create: create, update: update});
+var game = new Phaser.Game(GAME_HEIGHT,GAME_WIDTH, Phaser.AUTO, '',{ preload: preload, create: create, update: update});
 
 var updateTicker = 0;
 var varTicker = 0;
 var updateCycle = 1;
 var mapLower = null;
 var mapUpper = null;
+var darknessMask = null;
+
 var isCurveVar = 1;
 var isCurveVarTarget = 1;
 
 var isCurveSize = 0;
 var isCurveSizeTarget = 0;
 
+var isDarknessSize = 0;
+var isDarknessTarget = 0;
+
 var lowerLevelVar = 0
 
 function preload() {
-	game.load.spritesheet('player', 'img/player.png',32,32,4);
-	game.load.image('platform', 'img/platform.png');
+	game.load.image('lightmask', 'img/lightmask.png');
 }
 
 function create() {
@@ -33,39 +37,32 @@ function create() {
 	game.time.desiredFps = 60;
 	game.time.desiredFpsMult = 1/60;
 	game.time.events.loop(4000, randomizeNewCurve.bind(this));
-	game.time.events.loop(50, increaseCurveVarUntil.bind(this));
+	game.time.events.loop(40, increaseCurveVarUntil.bind(this));
 
 	game.time.events.loop(2000, randomizeNewCurveSize.bind(this));
-	game.time.events.loop(50, increaseCurveSizeUntil.bind(this));
-/*
+	game.time.events.loop(40, increaseCurveSizeUntil.bind(this));
 
-	game.physics.startSystem(Phaser.Physics.ARCADE);
-	player = new Player(game,64,20);
-	game.add.existing(player);
-	//Enable mouse button
-	game.input.mouse.capture = true;
-    game.physics.arcade.gravity.y = 400;
-	//Add a platforms group to hold all of our tiles, and create a bunch of them
-	game.time.events.add(1500,createObstacle);
 
-	scoreText = game.add.text(100, 64, "Score: "+currentScore.toString(),{font: "16px Arial Black"});
-	scoreText.fill = "white";
-   
+	game.time.events.loop(4000, randomizeNewDarkness.bind(this));
+	game.time.events.loop(40, setDarknessMask.bind(this));
 
-    highScoreListText.push(game.add.text(100, 16, "Top Score: ",{fill:"yellow", font: "20px Arial Black", align: "left"}));
-    setHighScore();*/
-    /*highScoreListText.push(game.add.text(250, 32, "2. 0",{fill:"white", font: "20px Arial Black"}));
-    highScoreListText.push(game.add.text(250, 48, "3. 0",{fill:"white", font: "20px Arial Black"}));
-    highScoreListText.push(game.add.text(250, 64, "4. 0",{fill:"white", font: "20px Arial Black"}));
-    highScoreListText.push(game.add.text(250, 80, "5. 0",{fill:"white", font: "20px Arial Black"}));*/
     mapLower = game.add.graphics(0,0);
     mapUpper = game.add.graphics(0,0);
-
-    // set a fill and line style
-   
+    darknessMask = game.add.graphics(0,0);
+    darknessMask.alpha = 0;
    
     
 }
+
+function addDarknessMask(){
+	mapUpper.mask = darknessMask;
+    mapLower.mask = darknessMask;
+}
+
+function removeDarknessMask(){
+	isDarknessTarget = 0;
+}
+
 function increaseCurveVarUntil(){
 	if(isCurveVar < isCurveVarTarget){
 		isCurveVar += 0.01
@@ -96,12 +93,40 @@ function increaseCurveSizeUntil(){
 	}
 }
 
+function setDarknessMask(){
+	if(isDarknessSize < isDarknessTarget){
+		isDarknessSize += 0.01
+		if(isDarknessSize > isDarknessTarget){
+			isDarknessSize = isDarknessTarget;
+		}
+	}
+	else if(isDarknessSize > isDarknessTarget){
+		isDarknessSize -= 0.01;
+		if(isDarknessSize < isDarknessTarget){
+			isDarknessSize = isDarknessTarget;
+			mapUpper.mask = null;
+			mapLower.mask = null;
+		}
+	}
+}
+
 function randomizeNewCurve(){
 	isCurveVarTarget = Math.random();
 }
 
 function randomizeNewCurveSize(){
 	isCurveSizeTarget = Math.random();
+}
+
+function randomizeNewDarkness(){
+	if(Math.floor(Math.random() * 2) === 0){
+		isDarknessSize = 0;
+		isDarknessTarget = 1;
+		addDarknessMask();
+	}
+	else{
+		removeDarknessMask();
+	}
 }
 
 function update() {
@@ -127,7 +152,9 @@ function update() {
 	mapLower.lineTo( GAME_WIDTH+300 , 800);
     mapLower.endFill();
 	
-
+	darknessMask.clear();
+    darknessMask.beginFill(0x000000);
+    darknessMask.drawCircle(500, 400, 1500 - (1000 * isDarknessSize));
 	
 }
 
