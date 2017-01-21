dfb3df8dfecf768018920e7152978e98fb67d569
rhalsas <rasmus.halsas@lut.fi>
2017-01-21 17:18:23 +0200

    -worm is bäk

--- client/js/player.js
+++ client/js/player.js
@@ -3,7 +3,11 @@ Player = function (game, x, y) {
     Phaser.Graphics.call(this, game, x, y);
 
     cursors = game.input.keyboard.createCursorKeys();
-    this.prevYCoordinates = [];
+
+     this.prevYCoordinates = [];
+    for(var i = 0; i < 50; i++){
+        this.prevYCoordinates.push(100);
+    }
     this.y_move = 0;
     this.prevSpotY = 0;
 
@@ -19,21 +23,47 @@ Player.prototype.constructor = Player;
 
 var movementSpeed = 500;
 
-
-
 Player.prototype.update = function() {
 
     this.clear();
-    this.lineStyle(10, 0xFF0000, 0.2);
- 
-    for(var x = 0; x < 100; x++){
-        this.lineTo(x,100 + (200 * (this.y_move * (x/100)) ));
+    this.moveTo(0, this.prevYCoordinates[0]);
+    this.lineStyle(10, 0xFF0000);
+
+    for(var y = 0; y < this.prevYCoordinates.length; y++){
+        if(this.prevYCoordinates[y+5]){
+            this.prevYCoordinates[y+4] = this.prevYCoordinates[y+5];
+            
+        } 
+        if(this.prevYCoordinates[y+4]){
+            this.prevYCoordinates[y+3] = this.prevYCoordinates[y+4];
+        
+        } 
+        if(this.prevYCoordinates[y+3]){
+            this.prevYCoordinates[y+2] = this.prevYCoordinates[y+3];
+        } 
+        if(this.prevYCoordinates[y+2]){
+            this.prevYCoordinates[y+1] = this.prevYCoordinates[y+2];
+        } 
+        if(this.prevYCoordinates[y+1]){
+            this.prevYCoordinates[y] = this.prevYCoordinates[y+1];
+        }
+        else {
+            this.prevYCoordinates[y] += this.y_move;
+        }
     }
+
+    for(var x = 1; x < 50; x++){
+        this.lineTo(x*4, this.prevYCoordinates[x]);
+    }
+ 
 
     if (cursors.up.isDown)
     {
         //  Move down
-        this.y_move -= 0.1;
+        this.y_move -= 0.75;
+        if(this.y_move < -10){
+            this.y_move = -10;
+        }
     }
     else if (cursors.left.isDown)
     {
@@ -46,6 +76,9 @@ Player.prototype.update = function() {
     else if (cursors.down.isDown)
     {
         //  Move up
-        this.y_move += 0.1;
+         this.y_move += 0.75;
+         if(this.y_move > 10){
+            this.y_move = 10;
+        }
     }
 };
