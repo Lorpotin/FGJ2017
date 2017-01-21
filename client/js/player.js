d53237fd198dab5fc4df9d1e5bc31030ef5bd745
Tatu Huttunen <tatu.huttunen@lut.fi>
2017-01-22 00:28:56 +0200

    sending player coordinates

--- client/js/player.js
+++ client/js/player.js
@@ -20,7 +20,9 @@ Player.prototype.constructor = Player;
  * Automatically called by World.update
  **/
 
-
+Player.prototype.getPrevYCoordinates = function() {
+    return this.prevYCoordinates;
+}
 Player.prototype.update = function() {
 
     this.clear();
